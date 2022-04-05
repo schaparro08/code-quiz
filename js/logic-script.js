89b01f0bf771fe6d85d2d317fed3cb0cbc4a37c3
Stephanie <101684009+schaparro08@users.noreply.github.com>
2022-04-05 19:29:15 -0400

    creation of  getAnswer function

--- js/logic-script.js
+++ js/logic-script.js
@@ -2,7 +2,7 @@
 // index at 0
 var questionsIndex = 0;
 // keep timer score for quiz
-var time = questions.length * 15;
+
 
 var timerId;
 
@@ -10,7 +10,7 @@ var timerId;
 var questionsEl = document.getElementById("questions");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("submit");
-var startBtn = document.getElementById("start");
+var startBtn = document.getElementById("startbtn");
 var initialsEl = document.getElementById("initials");
 var finalPage = document.getElementById("final-page");
 
@@ -41,6 +41,8 @@ var questions = [
 
 ];
 
+var time = questions.length * 15;
+
 //start quiz function 
 function beginQuiz() {
 var beginEl = document.getElementById("beginning");
@@ -55,10 +57,48 @@ timerEl.textContent = time;
 grabQuestions();
 
 }
-
+// Function to show the question on page
 function grabQuestions() {
+    if (questionsIndex < questions.length) {
 
+    // var to get current question array
+   var currentQuestion = questions[questionsIndex];
+   // empty out the old questions
+   document.querySelector("#choices").textContent = ""
+console.log(currentQuestion);
+// put the question on the page
+ document.querySelector("#q-title").textContent = currentQuestion.title;
+ // put the answers on the page
+ document.querySelector("#choices").removeAttribute("class");
+ for (let index = 0; index < currentQuestion.choices.length; index++) {
+    var button = document.createElement("button");
+     button.textContent = currentQuestion.choices[index];
+     button.classList.add("answer");
+     document.querySelector("#choices").appendChild(button);
+ }
 }
+else {
+    endgame();
+}
+}
+// Function will get answer off the button and compare it w the correct answer
+function getAnswer(event) {
+    if(event.target.matches(".answer")) {
+
+        console.log(event.target.textContent);
+        questionsIndex++;
+        grabQuestions();
+    }
+    
+}
+
+function endgame() {
+    // stop the timer
+    // get the text form the timer and make it the score
+    //show button that allows them to go to the final score page
+    
+}
+
 
 function clockTick() {
     time--;
@@ -66,7 +106,7 @@ function clockTick() {
 
     if (time <= 0) {
         // call back function to end quiz
-        
+
     }
 
 }
@@ -78,14 +118,5 @@ function clockTick() {
 
 
 
-
-
-
-
-
-
-
-
-
-
+document.querySelector(".frontpage").addEventListener("click", getAnswer)
 startBtn.addEventListener("click", beginQuiz);
